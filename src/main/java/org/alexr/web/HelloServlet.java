f18b84302e56189a828a6a5ca33abf36c799328d
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-02 22:38:07 +0400

    -- initial

---
+++ src/main/java/org/alexr/web/HelloServlet.java
@@ -0,0 +1,23 @@
+package org.alexr.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    try (PrintWriter w = resp.getWriter();) {
+      w.println("HelloServlet.GET");
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    try (PrintWriter w = resp.getWriter();) {
+      w.println("HelloServlet.POST");
+    }
+  }
+}
---
+++ src/main/java/org/alexr/web/MinimalWebApplication.java
@@ -0,0 +1,27 @@
+package org.alexr.web;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+/**
+ * http://localhost:5000
+ */
+public class MinimalWebApplication {
+  public static void main(String[] args) throws Exception {
+    int port;
+    try {
+      port = Integer.parseInt(System.getenv("PORT"));
+    } catch (NumberFormatException ex) {
+      port = 5000;
+    }
+    Server server = new Server(port);
+
+    ServletContextHandler handler = new ServletContextHandler();
+
+    handler.addServlet(HelloServlet.class, "/*");
+
+    server.setHandler(handler);
+    server.start();;
+    server.join();
+  }
+}
